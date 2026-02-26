289c5724c12404b82d93c925b059d521868bc76d
Aditya Manoli <manoliaditya@gmail.com>
2026-02-26 13:12:00 +0530

    Added Calculator logic

--- calc.py
+++ calc.py
@@ -1,1 +1,19 @@
-#nothign for now
+first_number = float(input("Enter the first number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+second_number = float(input("Enter the second number: "))
+
+if operation == "+":
+    result = first_number + second_number   
+elif operation == "-":
+    result = first_number - second_number
+elif operation == "*":
+    result = first_number * second_number
+elif operation == "/":
+    if second_number != 0:
+        result = first_number / second_number
+    else:
+        result = "Error: Division by zero is not allowed."
+else:
+    result = "Error: Invalid operation."    
+
+print("Result:", result)
